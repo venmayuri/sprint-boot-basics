33420ca59cf7b4be2416c9e2ce978b982e3c042f
venmayuri <venmayuri@gmail.com>
2019-02-14 22:00:17 -0800

    vp0214 this is my last commit

---
+++ src/main/java/com/example/demo/domain/Names.java
@@ -0,0 +1,17 @@
+package com.example.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Names {
+
+	private String name;
+	private String center;
+
+}
